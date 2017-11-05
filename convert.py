405f44c49e9ce458cc72047d2e9518173c0f4345
Ryan Rabello <ryan.s.rabello@gmail.com>
2017-11-04 23:06:16 -0700

    Finished Portland Uploads

--- convert.py
+++ convert.py
@@ -4,32 +4,28 @@ import numpy
 import sqlite3
 import shutil
 import code
-
+import pandas
 
 # CONFIG
 #Directory of photos
-originDir = "./Mask 2017/"
+originDir = "./Portland Mask/"
 exportDir = "./EXPORT/"
 emailFile = "emails.txt"
+csvFile = "Portland Mask.csv"
 
-startIndex = 718 #Index origin 0 (row # - 1)
-stop = 3192 # The id of the photo to stop at.
+startIndex = 1 #Index origin 0 (row # - 1)
 
 first = 0
 last = 1
 wwuid = 2
-year = 3
-gender = 4
-faculty = 5
-department = 6
-photo = 7
+photoIdx = 3
 
 # Import Data
 print('setting things up...')
-data = numpy.loadtxt(fname="id2photo.csv", delimiter=",", dtype=str)
-# remove headers
+data = pandas.io.parsers.read_csv(csvFile).as_matrix()
+data = data.astype(str)
+
 data = numpy.delete(data, numpy.arange(startIndex), 0)
-data = numpy.append(data, [["","","","","","","",str(stop)]], axis = 0)
 conn = sqlite3.connect('./databases/people.db')
 
 def wwuid2email(student):
@@ -65,30 +61,24 @@ print("Moving Files...")
 shutil.rmtree(exportDir, ignore_errors=True)
 shutil.copytree(originDir, exportDir)
 
-# Walla Walla Campus Mask
+# PORTLAND CAMPUS MASK photos
 
 print("Changing filenames...")
-for filename in os.listdir(exportDir):
-    os.rename(exportDir + filename, exportDir + filename[9:])
 
 for idx, val in enumerate(data):
-    current = int(val[photo])
-    if(not(data[idx + 1][photo])):
-        nextV = current + 1
-    else:
-        nextV = int(data[idx + 1][photo])
-    # TODO: change this to reflect end case
-    while (not(current >= nextV) and not(current > stop)):
+
+    photos = val[photoIdx].replace('"', '').replace("'","").split(', ')
+    for i, photoName in enumerate(photos):
+        # Debug info
+        print(exportDir + photoName + ".jpg   --->   " +  exportDir + photoName + "-" + val[wwuid] + ".jpg")
         try:
-            os.rename(exportDir + str("{0:0=5d}".format(current)) + ".jpg",exportDir + str("{0:0=5d}".format(current)) + "-" + str("{0:0=7d}".format(int(val[wwuid]))) + ".jpg")
+            os.rename(exportDir + photoName + ".jpg", exportDir + photoName + "-" + val[wwuid] + ".jpg")
         except OSError as err:
-            print("ERROR on ", current, ".jpg , ", err)
+            # code.interact(local=locals())
+            print("Photo ERROR", photoName, err)
             pass
         except:
-            print("ERROR on ", data[idx], current)
-            pass
-        #code.interact(local=locals())
-        current += 1
-
+            code.interact(local=locals())
+            print("ERROR on ", val)
 
 print("FINISHED")
